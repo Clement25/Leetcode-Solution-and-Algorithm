1b049d3f905a888c1b1405b4891601fe1debf445
Clement25 <35480362+Clement25@users.noreply.github.com>
2019-03-29 00:32:47 +0800

    modify format

--- Leetcode/python/20.py
+++ Leetcode/python/20.py
@@ -4,36 +4,36 @@ Created on Sun Sep 23 11:47:40 2018
 
 @author: DELL
 """
+
+
 class Solution:
-    def isValid(self,s):
-        dic={'(':')','[':']','{':'}'}
+    def isValid(self, s):
+        dic = {'(': ')', '[': ']', '{': '}'}
         stack = []
         for c in s:
-            if len(stack)==0:
+            if len(stack) == 0:
                 stack.append(c)
-            elif stack[-1] in [')',']','}']:
+            elif stack[-1] in [')', ']', '}']:
                 return False
-            elif c==dic[stack[-1]]:
+            elif c == dic[stack[-1]]:
                 stack.pop()
             else:
                 stack.append(c)
-        if len(stack)==0:
+        if len(stack) == 0:
             return True
         else:
             return False
-        
-    
+
 
 def main():
-    s='[]{}()'
-    s1="([)]"
+    s = '[]{}()'
+    s1 = "([)]"
     s2 = "[]}"
-    d = {1:2,2:3,3:4}
     key = Solution()
     print(s1[1:3])
     print(key.isValid(s))
     print(key.isValid(s2))
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
--- Leetcode/python/23s1.py
+++ Leetcode/python/23s1.py
@@ -4,10 +4,13 @@ Created on Sun Sep 23 13:17:58 2018
 
 @author: DELL
 """
+
+
 class ListNode:
-     def __init__(self, x):
-         self.val = x
-         self.next = None
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution:
     def mergeKLists(self, lists):
@@ -16,48 +19,53 @@ class Solution:
         :rtype: ListNode
         """
         l = len(lists)
-        if l==1:
+        if l == 1:
             return lists[0]
-        elif l>2:
-            mid = int (l/2)
-            return self.mergeKLists([self.mergeKLists(lists[0:mid]),self.mergeKLists(lists[mid:])])
+        elif l > 2:
+            mid = int(l / 2)
+            return self.mergeKLists([
+                self.mergeKLists(lists[0:mid]),
+                self.mergeKLists(lists[mid:])
+            ])
         else:
-            if lists[0].val<lists[1].val:
-                head=lists[0]
+            if lists[0].val < lists[1].val:
+                head = lists[0]
                 p2 = lists[1]
             else:
-                head=lists[1]
+                head = lists[1]
                 p2 = lists[0]
             p1 = head
             while p1.next is not None and p2 is not None:
-                if p2.val>=p1.val and p2.val<p1.next.val:
+                if p2.val >= p1.val and p2.val < p1.next.val:
                     temp = p2.next
-                    p2.next=p1.next
-                    p1.next=p2
-                    p1=p1.next
-                    p2=temp
+                    p2.next = p1.next
+                    p1.next = p2
+                    p1 = p1.next
+                    p2 = temp
                 else:
-                    p1=p1.next
+                    p1 = p1.next
             if p2 is not None:
-                p1.next=p2
+                p1.next = p2
             return head
-        
+
+
 def main():
     head1 = ListNode(3)
-    head1.next=ListNode(5)
-    head1.next.next=ListNode(7)
+    head1.next = ListNode(5)
+    head1.next.next = ListNode(7)
     head2 = ListNode(4)
-    head2.next=ListNode(6)
-    head2.next.next=ListNode(8)
-    head3=ListNode(2)
-    head3.next=ListNode(4)
-    head3.next=ListNode(8)
-    s=Solution()
-    f=s.mergeKLists([head1,head2,head3])
+    head2.next = ListNode(6)
+    head2.next.next = ListNode(8)
+    head3 = ListNode(2)
+    head3.next = ListNode(4)
+    head3.next = ListNode(8)
+    s = Solution()
+    f = s.mergeKLists([head1, head2, head3])
     p = f
     while p is not None:
         print(p.val)
-        p=p.next
+        p = p.next
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
